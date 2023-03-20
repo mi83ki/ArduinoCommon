59a88188f9b1ff6e0587a23abac6145c6a2f1e08
Tatsuya Miyazaki <miyazaki.tatsuya.mail@gmail.com>
2023-03-20 15:20:36 +0900

    [LinearChanger] 線形増減させるクラスを追加

---
+++ src/LinearChanger.cpp
@@ -0,0 +1,203 @@
+/**
+ * @file LinearChanger.cpp
+ * @brief 値を線形で増減させる
+ * @author Tatsuya Miyazaki
+ * @date 2023/3/19
+ *
+ * @details 例えば速度を徐々に上げたい場合などに使用するクラス
+ * @note 浮動小数点演算ではなく、固定小数点演算(fix.hpp)を行います（処理の高速化のため）
+ */
+
+#include "LinearChanger.h"
+
+/**
+ * コンストラクタ
+ * @param time 実行周期[ms]
+ * @param increase 加速度
+ * @param decrease 減速度
+ */
+LinearChanger::LinearChanger(uint16_t time, float increase, float decrease)
+{
+  _present = 0;
+  _target = 0;
+  _cycleTime = INT_TO_FIX(time) / 1000;
+  _increase = FLOAT_TO_FIX(increase);
+  _decrease = FLOAT_TO_FIX(decrease);
+}
+
+/**
+ * @brief Destroy the Linear Increaser:: Linear Increaser object
+ */
+LinearChanger::~LinearChanger()
+{
+  ;
+}
+
+/**
+ * @brief 増加が減少かを判断し、増減量を返す
+ *
+ * @return fix 増減量を返す
+ */
+fix LinearChanger::getIncrease(void)
+{
+  if (_present == _target)
+  {
+    return (0);
+  }
+  else if (_target == 0)
+  {
+    return (_decrease);
+  }
+  else if (_target > 0)
+  {
+    if (_present < _target)
+    {
+      return (_increase);
+    }
+    else
+    {
+      return (_decrease);
+    }
+  }
+  else
+  {
+    if (_present > _target)
+    {
+      return (_increase);
+    }
+    else
+    {
+      return (_decrease);
+    }
+  }
+  return (0);
+}
+
+/**
+ * @brief 値を増減させる（コンストラクタで設定した実行周期で実行すること）
+ *
+ * @return fix 更新した値
+ */
+fix LinearChanger::updateFix(void)
+{
+  fix increase = getIncrease();
+  if (_present < _target)
+  {
+    _present += FIX_MUL(increase, _cycleTime);
+    if (_present > _target)
+    {
+      _present = _target;
+    }
+  }
+  else if (_present > _target)
+  {
+    _present -= FIX_MUL(increase, _cycleTime);
+    if (_present < _target)
+    {
+      _present = _target;
+    }
+  }
+  // Serial.println("updateFix: " + String(_target) + ", " + String(_present) + ", " + String(_increase));
+  return (_present);
+}
+
+/**
+ * @brief 値を増減させる（コンストラクタで設定した実行周期で実行すること）
+ *
+ * @return fix 更新した値
+ */
+float LinearChanger::update(void)
+{
+  return FIX_TO_FLOAT(updateFix());
+}
+
+/**
+ * @brief 目標値を設定する
+ *
+ * @param target 目標値
+ */
+void LinearChanger::setTargetFix(fix target)
+{
+  _target = target;
+}
+
+/**
+ * @brief 目標値を設定する
+ *
+ * @param target 目標値
+ */
+void LinearChanger::setTarget(float target)
+{
+  setTargetFix(FLOAT_TO_FIX(target));
+}
+
+/**
+ * @brief 目標値を取得する
+ *
+ * @return fix
+ */
+fix LinearChanger::getTargetFix(void)
+{
+  return (_target);
+}
+
+/**
+ * @brief 目標値を取得する
+ *
+ * @return float
+ */
+float LinearChanger::getTarget(void)
+{
+  return FIX_TO_FLOAT(getTargetFix());
+}
+
+/**
+ * @brief 現在値を取得する
+ *
+ * @return fix
+ */
+fix LinearChanger::getPresentFix(void)
+{
+  return (_present);
+}
+
+/**
+ * @brief 現在値を取得する
+ *
+ * @return float
+ */
+float LinearChanger::getPresent(void)
+{
+  return FIX_TO_FLOAT(getPresentFix());
+}
+
+/**
+ * @brief 目標値に到達したかどうか
+ *
+ * @return true 目標値に到達
+ * @return false 目標値に未達
+ */
+bool LinearChanger::isTarget(void)
+{
+  return _present == _target;
+}
+
+/**
+ * @brief 単位時間当たりの増加量を設定する
+ *
+ * @param increase 単位時間当たりの増加量
+ */
+void LinearChanger::setIncrease(float increase)
+{
+  _increase = FLOAT_TO_FIX(increase);
+}
+
+/**
+ * @brief 単位時間当たりの減少量を設定する
+ *
+ * @param decrease 単位時間当たりの減少量
+ */
+void LinearChanger::setDecrease(float decrease)
+{
+  _decrease = FLOAT_TO_FIX(decrease);
+}
---
+++ src/LinearChanger.h
@@ -0,0 +1,47 @@
+/**
+ * @file LinearChanger.h
+ * @brief 値を線形で増減させる
+ * @author Tatsuya Miyazaki
+ * @date 2023/3/19
+ *
+ * @details 例えば速度を徐々に上げたい場合などに使用するクラス
+ * @note 浮動小数点演算ではなく、固定小数点演算(fix.hpp)を行います（処理の高速化のため）
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+#include "fix.hpp"
+
+class LinearChanger
+{
+public:
+  LinearChanger(uint16_t, float, float);
+  ~LinearChanger();
+  fix updateFix(void);
+  float update(void);
+  void setTargetFix(fix);
+  void setTarget(float);
+  fix getTargetFix(void);
+  float getTarget(void);
+  fix getPresentFix(void);
+  float getPresent(void);
+  bool isTarget(void);
+  void setIncrease(float);
+  void setDecrease(float);
+
+private:
+  fix getIncrease(void);
+
+  /** 現在値 */
+  fix _present;
+  /** 目標値 */
+  fix _target;
+  /** 単位時間当たりの増加量 */
+  fix _increase;
+  /** 単位時間当たりの減少量 */
+  fix _decrease;
+  /** 処理周期[ms] */
+  fix _cycleTime;
+};
